a901e7f8ab6fab646455f9ddabe824e7f9a40e6b
Surya Sharma <surya@surya.zone>
2022-06-20 18:13:08 -0500

    removed secret key

--- pridebot/settings.py
+++ pridebot/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-8li80i&_(f8d$b$q+_4t-o5b75)+nzfdd2^de_qn^c0g*8=9w*"
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get("DEBUG") == "True"
